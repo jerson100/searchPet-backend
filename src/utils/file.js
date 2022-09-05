28f24242244258fd3b4028f9a2ecfbe03899d088
jerson100 <juamkoo@gmail.com>
2022-09-04 23:18:09 -0500

    Modificamos el archivo file

---
+++ src/utils/file.js
@@ -0,0 +1,41 @@
+const fs = require("fs-extra");
+
+const removeFilesFromSystem = (files) => {
+    files?.forEach(f => { fs.remove(f.tempFilePath) });
+}
+
+const removeFilesFromObject = (objFiles) => {
+    if(!objFiles) return;
+    const files = toFileArray(objFiles);
+    removeFilesFromSystem(files);
+}
+
+/*
+* {
+*   img: {}
+* }
+*
+* {
+*   img: [
+*       {},
+*       {}
+*   ]
+* }
+* */
+const toFileArray = (objFiles={}) => {
+    const files = [];
+    for(let file in objFiles){
+        if(!Array.isArray(objFiles[file])){
+            files.push(objFiles[file]);
+        }else{
+            files.push(...objFiles[file]);
+        }
+    }
+    return files;
+}
+
+module.exports = {
+    removeFilesFromObject,
+    removeFilesFromSystem,
+    toFileArray
+}
