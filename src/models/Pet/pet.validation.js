fd32eac6d908905a4cec8dc3cdf11ed56fa50585
jerson100 <juamkoo@gmail.com>
2022-09-07 15:34:34 -0500

    Mejorando el schema de validación para la uri pet

--- src/models/Pet/pet.validation.js
+++ src/models/Pet/pet.validation.js
@@ -3,12 +3,13 @@ const Joi = require("joi").extend(JoiDate);
 
 const CreatePetSchemaValidation = Joi.object({
     name: Joi.string().min(2).max(40).required(),
-    breed: Joi.string().regex(/^[a-fA-F\d]{24}$/),
+    breed: Joi.string().regex(/^[a-fA-F\d]{24}$/).required(),
     dateOfBirth: Joi.date().format("DD/MM/YYYY"),
+    description: Joi.string().max(400).empty(""),
     characteristics: Joi.object({
         size: Joi.string().valid("Pequeño","Mediano","Grande"),
-        eyeColor: Joi.string().min(2).max(40),
-        hairColor: Joi.string().min(2).max(40)
+        eyeColor: Joi.string().regex(/[a-z ]{2,40}/i).empty("").trim(),
+        hairColor: Joi.string().regex(/[a-z ]{2,40}/i).empty("").trim(),
     }),
 });
 
@@ -17,10 +18,11 @@ const PutPetSchemaValidation = Joi.object({
     breed: Joi.string().regex(/^[a-fA-F\d]{24}$/).required(),
     urlImageProfile: Joi.string().required(),
     dateOfBirth: Joi.date().format("DD/MM/YYYY").required(),
+    description: Joi.string().max(400).allow(""),
     characteristics: Joi.object({
         size: Joi.string().valid("Pequeño","Mediano","Grande"),
-        eyeColor: Joi.string().min(2).max(40),
-        hairColor: Joi.string().min(2).max(40)
+        eyeColor: Joi.string().regex(/[a-z ]{2,40}/i).allow("").trim(),
+        hairColor: Joi.string().regex(/[a-z ]{2,40}/i).allow("").trim(),
     }).required(),
 });
 
@@ -28,10 +30,11 @@ const PatchPetSchemaValidation = Joi.object({
     name: Joi.string().min(2).max(40),
     breed: Joi.string().regex(/^[a-fA-F\d]{24}$/),
     dateOfBirth: Joi.date().format("DD/MM/YYYY"),
+    description: Joi.string().max(400).allow(""),
     characteristics: Joi.object({
         size: Joi.string().valid("Pequeño","Mediano","Grande"),
-        eyeColor: Joi.string().min(2).max(40),
-        hairColor: Joi.string().min(2).max(40)
+        eyeColor: Joi.string().regex(/[a-z ]{2,40}/i).allow("").trim(),
+        hairColor: Joi.string().regex(/[a-z ]{2,40}/i).allow("").trim(),
     }),
 });
 
