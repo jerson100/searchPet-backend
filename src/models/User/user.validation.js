77447650f476d8b8b0699388b6d34cb0b4fdadcd
jerson100 <juamkoo@gmail.com>
2022-09-08 14:48:20 -0500

    Mejorando el esquema de validación para el recurso usuairo

--- src/models/User/user.validation.js
+++ src/models/User/user.validation.js
@@ -1,13 +1,38 @@
 const Joi = require("joi").extend(require("@joi/date"))
 
-const obj = {
-    name: Joi.string().min(2).max(30).required(),
-    paternalSurname: Joi.string().min(2).max(30).required(),
-    maternalSurname: Joi.string().min(2).max(30).required(),
+const UserCreationSchemaValidation = Joi.object({
+    name: Joi.string().min(2).max(30).required().trim(),
+    paternalSurname: Joi.string().regex(/^[a-z]+$/i).min(2).max(30).required().trim(),
+    maternalSurname: Joi.string().regex(/^[a-z]+$/i).min(2).max(30).required().trim(),
     birthday: Joi.date().format("DD/MM/YYYY"),
-    username: Joi.string().regex(/[a-z\d]+/).required(),
+    username: Joi.string().regex(/^[a-z\d]+$/i).required(),
     email: Joi.string().email().required(),
-    address: Joi.string().min(2).max(100),
+    address: Joi.string().min(2).max(100).empty("").trim(),
+    location: Joi.object({
+        latitud: Joi.number().required(),
+        longitud: Joi.number().required()
+    }).required(),
+    //Mínimo 10 y máximo 20 caracteres, al menos una letra mayúscula,
+    //una letra minúscula, un número y un carácter especial:
+    password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,20}$/).required(),
+    cPassword: Joi.string().equal(Joi.ref("password")).required(),
+    district: Joi.string().regex(/^[a-fA-F\d]{24}$/),
+    socialNetWorks: Joi.object({
+        facebook: Joi.string().empty(""),
+        twitter: Joi.string().empty(""),
+        instagram: Joi.string().empty(""),
+        whatsapp: Joi.string().empty("")
+    })
+});
+
+const UserUpdateSchemaValidation = Joi.object({
+    name: Joi.string().min(2).max(30).required().trim(),
+    paternalSurname: Joi.string().min(2).max(30).required().trim(),
+    maternalSurname: Joi.string().min(2).max(30).required().trim(),
+    birthday: Joi.date().format("DD/MM/YYYY"),
+    username: Joi.string().regex(/^[a-z\d]+$/i).required(),
+    email: Joi.string().email().required(),
+    address: Joi.string().min(2).max(100).allow("").trim(),
     location: Joi.object({
         latitud: Joi.number().required(),
         longitud: Joi.number().required()
@@ -17,26 +42,21 @@ const obj = {
     password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,20}$/).required(),
     district: Joi.string().regex(/^[a-fA-F\d]{24}$/),
     socialNetWorks: Joi.object({
-        facebook: Joi.string(),
-        twitter: Joi.string(),
-        instagram: Joi.string(),
-        whatsapp: Joi.string()
-    }),
-    urlImageProfile: Joi.string()
-}
-
-const UserCreationSchemaValidation = Joi.object(obj);
-
-const UserUpdateSchemaValidation = Joi.object(obj);
+        facebook: Joi.string().allow(""),
+        twitter: Joi.string().allow(""),
+        instagram: Joi.string().allow(""),
+        whatsapp: Joi.string().allow("")
+    })
+});
 
 const PatchUserUpdateSchemaValidation = Joi.object({
-    name: Joi.string().min(2).max(30),
-    paternalSurname: Joi.string().min(2).max(30),
-    maternalSurname: Joi.string().min(2).max(30),
-    birthday: Joi.date().format("DD/MM/YYYY"),
+    name: Joi.string().min(2).max(30).trim(),
+    paternalSurname: Joi.string().min(2).max(30).trim(),
+    maternalSurname: Joi.string().min(2).max(30).trim(),
+    birthday: Joi.date().format("DD/MM/YYYY").allow(""),
     username: Joi.string().regex(/[a-z\d]+/),
     email: Joi.string().email(),
-    address: Joi.string().min(2).max(100),
+    address: Joi.string().min(2).max(100).trim().allow(""),
     location: Joi.object({
         latitud: Joi.number(),
         longitud: Joi.number()
@@ -46,12 +66,11 @@ const PatchUserUpdateSchemaValidation = Joi.object({
     password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,20}$/),
     district: Joi.string().regex(/^[a-fA-F\d]{24}$/),
     socialNetWorks: Joi.object({
-        facebook: Joi.string(),
-        twitter: Joi.string(),
-        instagram: Joi.string(),
-        whatsapp: Joi.string()
+        facebook: Joi.string().allow(""),
+        twitter: Joi.string().allow(""),
+        instagram: Joi.string().allow(""),
+        whatsapp: Joi.string().allow("")
     }),
-    urlImageProfile: Joi.string()
 });
 
 const LoginSchemaValidation = Joi.object({
